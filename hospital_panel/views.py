812462bf60e5ccba62617df313cad7fe3331857e
SaeedReza Ghazanfari <saeedreza.gh.1397@gmail.com>
2022-12-08 14:51:11 +0330

    base of four page in panel

--- hospital_panel/views.py
+++ hospital_panel/views.py
@@ -2,9 +2,32 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class PanelHomePage(LoginRequiredMixin, generic.TemplateView):
+# url: /panel
+class HomePage(LoginRequiredMixin, generic.TemplateView):
     template_name = 'panel/home.html'
 
     def get_context_data(self, **kwargs):
-        context = super(PanelHomePage, self).get_context_data(**kwargs)
+        context = super(HomePage, self).get_context_data(**kwargs)
         return context
+
+
+# url: /panel/online-appointment
+class OnlineAppointmentPage(generic.TemplateView):
+    template_name = 'panel/online-appointment.html'
+
+
+# url: /panel/activate-account
+class ActivateAccountPage(generic.TemplateView):
+    template_name = 'panel/activate-account.html'
+
+
+# url: /panel/edit-info
+class EditInfoPage(generic.TemplateView):
+    template_name = 'panel/edit-info.html'
+
+
+# url: /panel/reset-password
+class ResetPassWordPage(generic.TemplateView):
+    template_name = 'panel/reset-password.html'
+
+
