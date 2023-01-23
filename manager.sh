2cb0047bb20872980e1c47ae654b639ba792ecd8
SaeedReza Ghazanfari <saeedreza.gh.1397@gmail.com>
2023-01-23 21:35:28 +0330

    edit manager.sh and add about us

--- manager.sh
+++ manager.sh
@@ -4,13 +4,41 @@
 # this file must be in root dir of django application #
 
 
+# define color functions
+black() { 
+	echo "\e[30m${1}\e[0m" 
+}
+red() { 
+	echo "\e[31m${1}\e[0m" 
+}
+green() { 
+	echo "\e[32m${1}\e[0m" 
+}
+yellow() { 
+	echo "\e[33m${1}\e[0m" 
+}
+blue() { 
+	echo "\e[34m${1}\e[0m" 
+}
+magenta() { 
+	echo "\e[35m${1}\e[0m" 
+}
+cyan() { 
+	echo "\e[36m${1}\e[0m" 
+}
+gray() { 
+	echo "\e[90m${1}\e[0m" 
+}
+
+clear
 echo 
 echo "# 1 => run server"
 echo "# 2 => git add and commit"
 echo "# 3 => make migrations and migrate db"
 echo "# 4 => remove all migrations files"
 echo "# 5 => remove all pycache files"
-echo "# 6 => exit from manager.sh"
+echo "# 6 => about manager.sh"
+echo "# 7 => exit from manager.sh"
 echo
 read -p "Enter your Selection: " FLAG
 echo
@@ -19,6 +47,7 @@ echo
 # run django server on port 8000
 if [ $FLAG = 1 ]; then
 	
+	clear
 	. venv/bin/activate
 	python3 manage.py runserver 8000
 
@@ -26,6 +55,7 @@ if [ $FLAG = 1 ]; then
 # git add . and git commit -m "$"
 elif [ $FLAG = 2 ]; then
 	
+	clear
 	echo "Enter your comment of this Commit:"
 	read COMMENT
 
@@ -37,6 +67,7 @@ elif [ $FLAG = 2 ]; then
 # makemigrations and migrate db
 elif [ $FLAG = 3 ]; then
 	
+	clear
 	read -p "Your selection is 'Make Migrations & Migrate', Are you Sure? [n/Y] " SUREMENT
 
 	if [ $SUREMENT = 'Y' ]; then
@@ -54,6 +85,7 @@ elif [ $FLAG = 3 ]; then
 # delete all of 00*.py files of migrations dir
 elif [ $FLAG = 4 ]; then
 	
+	clear
 	read -p "Your selection is 'Remove Migration Files', Are you Sure? [n/Y] " SUREMENT
 
 	if [ $SUREMENT = 'Y' ]; then
@@ -77,6 +109,7 @@ elif [ $FLAG = 4 ]; then
 # delete all of *.pyc files in this dir
 elif [ $FLAG = 5 ]; then
 
+	clear
 	read -p "Your selection is 'Remove PyCache Files', Are you Sure? [n/Y] " SUREMENT
 
 	if [ $SUREMENT = 'Y' ]; then
@@ -102,9 +135,32 @@ elif [ $FLAG = 5 ]; then
 		echo "Exited!"
 	fi
 
-# exit from manager.sh
+
+# about manager.sh
 elif [ $FLAG = 6 ]; then
 
+	clear
+	sleep 1
+	echo
+	cyan "This script is for all django applications."
+	cyan "If you can use shis, just add this script to root of application."
+	cyan "Open terminal and go to dir of your application. then, run the bellow command:"
+	echo
+	red "$ sh manager.sh"
+	echo	
+	cyan "Use from it and be Fun :)"
+	echo
+	sleep 0.5
+	cyan "Creator: Peaka"
+	sleep 0.5
+	cyan "Github: https://github.com/saeedrezaghazanfari/"
+	cyan "MyId in tel, insta: @SaeedReza_WU"
+	echo
+
+# exit from manager.sh
+elif [ $FLAG = 7 ]; then
+
+	clear
 	echo "have good time!"
 
 else
