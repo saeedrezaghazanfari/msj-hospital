2d2ac8d2b82807bb715d511eb54de04489070ec4
SaeedReza Ghazanfari <saeedreza.gh.1397@gmail.com>
2023-02-20 21:57:25 +0330

    add navbar templatetag

--- hospital_website/templatetags/web_tags.py
+++ hospital_website/templatetags/web_tags.py
@@ -1,12 +1,42 @@
 from django import template
 from hospital_blog.models import MedicalNoteModel
+from hospital_units.models import UnitModel
 
 
 register = template.Library()
 
 @register.simple_tag
 def doctor_notes():
-    notes = MedicalNoteModel.objects.filter(is_active=True).all()[:6]
-    if notes:
-        return notes
+    data = MedicalNoteModel.objects.filter(is_active=True).all()[:6]
+    if data:
+        return data
     return None
+
+@register.simple_tag
+def list_clinics():
+    data = UnitModel.objects.filter(subunit__category='medical', subunit__title_fa='درمانگاه').all()
+    if data:
+        return data
+    return None
+
+@register.simple_tag
+def list_paraclinics():
+    data = UnitModel.objects.filter(subunit__category='paraclinic').all()
+    if data:
+        return data
+    return None
+
+@register.simple_tag
+def list_officials():
+    data = UnitModel.objects.filter(subunit__category='official').all()
+    if data:
+        return data
+    return None
+
+@register.simple_tag
+def list_medicals():
+    data = UnitModel.objects.filter(subunit__category='medical').all()
+    if data:
+        return data
+    return None
+
