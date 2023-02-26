99053d4f69534d2b034b5380d736e9a31f246182
SaeedReza Ghazanfari <saeedreza.gh.1397@gmail.com>
2023-02-26 17:26:25 +0330

    write aboutus view

--- hospital_website/urls.py
+++ hospital_website/urls.py
@@ -4,9 +4,31 @@ from . import views
 
 app_name = 'website'
 urlpatterns = [
+
+    # real
     path('', views.home_page, name='home'),
-    path('about-us/', views.aboutus_page, name='aboutus'),
-    # path('history/', views.history_page, name='history'),
     path('search/', views.search_page, name='search'),
     path('newsletter/register/', views.newsletter_page, name='email-newsletter'),
+
+    # about-us tab
+    path('about-us/', views.aboutus_page, name='aboutus'),
+    path('history/', views.history_page, name='history'),
+    path('workshops/', views.workshops_page, name='workshops'),
+    path('certificates/', views.certificates_page, name='certificates'),
+    path('gallery/images/', views.gallery_imgs_page, name='gallery-imgs'),
+    path('gallery/videos/', views.gallery_vids_page, name='gallery-vids'),
+    path('policies/', views.policies_page, name='policies'),
+    path('management/', views.management_page, name='management'),
+    path('chart/', views.chart_page, name='chart'),
+    path('targets/', views.targets_page, name='targets'),
+    path('perspective/', views.perspective_page, name='perspective'),
+    path('visiting-famous-faces/', views.visiting_famous_page, name='visiting-famous'),
+    path('patient-chart/', views.patient_chart_page, name='patient-chart'),
+    path('committees/', views.committees_page, name='committees'),
+    path('credit/', views.credit_page, name='credit'),
+    path('quality-improvement/', views.quality_improvement_page, name='quality-improvement'),
+    path('deceaseds/', views.deceaseds_page, name='deceaseds'),
+
+    # patient guidence tab
+
 ]
--- hospital_website/views.py
+++ hospital_website/views.py
@@ -1,8 +1,12 @@
+from django.utils import timezone
 from django.shortcuts import render, redirect
 from django.utils.translation import get_language
 from django.utils.translation import gettext_lazy as _
 from django.contrib import messages
-from hospital_setting.models import NewsLetterEmailsModel, FAQModel
+from hospital_setting.models import (
+    NewsLetterEmailsModel, FAQModel, SettingModel
+)
+from hospital_contact.models import WorkshopModel
 from extentions.utils import is_email, write_action
 
 
@@ -50,6 +54,97 @@ def newsletter_page(request):
 
 # url: /about-us/
 def aboutus_page(request):
-    return render(request, 'web/aboutus.html')
+
+    return render(request, 'web/aboutus.html', {
+        'setting': SettingModel.objects.first() if SettingModel.objects.exists() else None
+    })
+
+
+# url: /history/
+def history_page(request):
+
+    return render(request, 'web/history.html', {
+        'history': SettingModel.objects.first().history if SettingModel.objects.exists() else None
+    })
+
+
+# url: /workshops/
+def workshops_page(request):
+
+    return render(request, 'web/workshops.html', {
+        'active_workshops': WorkshopModel.objects.filter(start_date__gt=timezone.now()).all(),
+        'prevs_workshops': WorkshopModel.objects.filter(start_date__lt=timezone.now()).all(),
+    })
+
+
+# url: /certificates/
+def certificates_page(request):
+    return render(request, 'web/certificates.html', {})
+
+
+# url: /gallery/images/
+def gallery_imgs_page(request):
+    return render(request, 'web/gallery-imgs.html', {})
+
+
+# url: /gallery/videos/
+def gallery_vids_page(request):
+    return render(request, 'web/gallery-vids.html', {})
+
+
+# url: /policies
+def policies_page(request):
+    return render(request, 'web/policies.html', {})
+
+
+# url: /management
+def management_page(request):
+    return render(request, 'web/management.html', {})
+
+
+# url: /chart
+def chart_page(request):
+    return render(request, 'web/chart.html', {})
+
+
+# url: /targets
+def targets_page(request):
+    return render(request, 'web/targets.html', {})
+
+
+# url: /perspective
+def perspective_page(request):
+    return render(request, 'web/perspective.html', {})
+
+
+# url: /visiting-famous-faces
+def visiting_famous_page(request):
+    return render(request, 'web/visiting-famous-faces.html', {})
+
+
+# url: /patient-chart
+def patient_chart_page(request):
+    return render(request, 'web/patient-chart.html', {})
+
+
+# url: /committees
+def committees_page(request):
+    return render(request, 'web/committees.html', {})
+
+
+# url: /credit
+def credit_page(request):
+    return render(request, 'web/credit.html', {})
+
+
+# url: /quality-improvement
+def quality_improvement_page(request):
+    return render(request, 'web/quality-improvement.html', {})
+
+
+# url: /deceaseds
+def deceaseds_page(request):
+    return render(request, 'web/deceaseds.html', {})
+
 
 
